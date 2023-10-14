fd9639f5d8eda7ef6a2808a9879a67278f1e0aef
Igor Feoktistov <feoktist92@gmail.com>
2023-10-14 13:00:25 +0800

    styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,53 @@
+document.getElementById('main-action-button').onclick = function () {
+    document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+};
 
-document.getElementById("main-action-button").onclick= function () {
-    document.getElementById("products").scrollIntoView({behavior:"smooth"})}
-
-let links = document.querySelectorAll(".menu-item > a");
-for (let i= 0; i < links.length; i++) {
+let links = document.querySelectorAll('.menu-item > a');
+for (let i = 0; i < links.length; i++) {
     links[i].onclick = function () {
-        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"})}
+        document
+            .getElementById(links[i].getAttribute('data-link'))
+            .scrollIntoView({ behavior: 'smooth' });
+    };
 }
 
-
-let buttons = document.getElementsByClassName("product-button");
-for (let i= 0; i < buttons.length; i++) {
+let buttons = document.getElementsByClassName('product-button');
+for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
-        document.getElementById("order").scrollIntoView({behavior:"smooth"})}
+        document.getElementById('order').scrollIntoView({ behavior: 'smooth' });
+    };
 }
 
-
-let burger = document.getElementById("burger");
-let name = document.getElementById("name");
-let phone = document.getElementById("phone");
-document.getElementById("order-action").onclick = function () {
+let burger = document.getElementById('burger');
+let surname = document.getElementById('name');
+let phone = document.getElementById('phone');
+document.getElementById('order-action').onclick = function () {
     let hasError = false;
-    [burger, name, phone].forEach(item =>{
-        if(!item.value) {
-            item.parentElement.style.background = "red";
+    [burger, surname, phone].forEach((item) => {
+        if (!item.value) {
             hasError = true;
-        } else {item.parentElement.style.background = ""
+            item.parentElement.style.background = 'red';
+        } else {
+            item.parentElement.style.background = '';
         }
     });
     if (!hasError) {
-        [burger, name, phone].forEach(item =>{
-          item.value = "";
+        [burger, surname, phone].forEach((item) => {
+            item.value = '';
         });
-        alert("Спасибо за заказ! Мы скоро свяжемся с вами!");
+        alert('Спасибо за заказ! Мы скоро свяжемся с вами!');
     }
-}
+};
 
-
-let prices = document.getElementsByClassName("products-item-price");
-document.getElementById("change-currency").onclick = function (e) {
+let prices = document.getElementsByClassName('products-item-price');
+document.getElementById('change-currency').onclick = function (e) {
     let currentCurrency = e.target.innerText;
-    let newCurrency = "$";
+    let newCurrency = '$';
     let coefficient = 1;
-    if (currentCurrency === "$") {
-        newCurrency = "₽";
+    if (currentCurrency === '$') {
+        newCurrency = '₽';
         coefficient = 77;
-    } else if (currentCurrency === "₽") {
-        newCurrency = "BYN";
-        coefficient = 3;
-    } else if (currentCurrency === 'BYN') {
+    } else if (currentCurrency === '₽') {
         newCurrency = '€';
         coefficient = 0.9;
     } else if (currentCurrency === '€') {
@@ -57,10 +56,11 @@ document.getElementById("change-currency").onclick = function (e) {
     }
     e.target.innerText = newCurrency;
     for (let i = 0; i < prices.length; i++) {
-        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+        prices[i].innerText =
+            +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(
+                1
+            ) +
+            ' ' +
+            newCurrency;
     }
-}
-
-
-
-
+};
